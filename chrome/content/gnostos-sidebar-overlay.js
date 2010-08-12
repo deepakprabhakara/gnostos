0401144095903259ac1d61896a613bad5376b38b
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2010-08-12 04:08:05 +0200

    Time to move to the server, start building the backend now

--- chrome/content/gnostos-sidebar-overlay.js
+++ chrome/content/gnostos-sidebar-overlay.js
@@ -1,6 +1,9 @@
 var gnostos = { 
 	mSideBar: null,
-	mDocumentElement: null,
+	mAccountEmailAddress: null,
+	mAccountDisplayName: null,
+	//mSideBarDocumentHead: null,
+	//mSideBarDocumentBody: null,
 	mEmailAddress: null,
 	mDisplayName: null,
 }
@@ -39,61 +42,10 @@ gnostos.onMessagePaneLoad = function()
 	}
 
 	if(gnostos.mSideBar)
-	{		
-		gnostos.mDocumentElement = gnostos.mSideBar.contentDocument.documentElement;
-		gnostos.mDocumentElement.innerHTML = "<head><title></title></head><body>Loading</body>";
-		gDebugger.log(gnostos.mDocumentElement.innerHTML);
-		if (window.XMLHttpRequest)
-		{
-			var xmlhttp=new XMLHttpRequest();
-			xmlhttp.open("GET","http://friendfeed.com/api/feed/user?emails="+gnostos.mEmailAddress+"&format=xml&num=2",true);
-			xmlhttp.onprogress = gnostos.onXHRProgress;
-			xmlhttp.onload = gnostos.onXHRLoad;
-			xmlhttp.onerror = gnostos.onXHRError;
-			xmlhttp.onreadystatechange = gnostos.onXHRReadyStateChange;
-			xmlhttp.send();
-		}
-		//gnostosSideBar.setAttribute('src', "http://friendfeed.com/api/feed/user?emails="+emailAddress+"&format=xml&num=2");
+	{
+		//gnostos.mSideBar.setAttribute('src', "http://localhost/user?emails="+emailAddress+"&format=xml");
+		gnostos.mSideBar.setAttribute('src', "file://localhost/C:/Deepak/Desktop/new.html");
 	}
-}
-
-gnostos.onXHRProgress = function(e)
-{
-	var percentComplete = (e.position / e.totalSize)*100;
-	gDebugger.log("Progress: " + percentComplete);
-	if(percentComplete / 33 == 1)
-		gnostos.mDocumentElement.innerHTML = "<head><title></title></head><body>Loading.</body>";
-	else if(percentComplete / 33 == 2)
-		gnostos.mDocumentElement.innerHTML = "<head><title></title></head><body>Loading..</body>";
-	else if(percentComplete / 33 == 3)
-		gnostos.mDocumentElement.innerHTML = "<head><title></title></head><body>Loading...</body>";
-}
-
-gnostos.onXHRLoad = function(e)
-{
-	 gDebugger.log(this.responseText);
-	 gnostos.mDocumentElement.innerHTML = "<head><title></title></head><body>";
-	 //gDebugger.log(this.responseXML);
-	 var xmlDoc = this.responseXML;
-	 var titleElements = xmlDoc.getElementsByTagName("title");
-	 
-	 for(var i = 0; i < titleElements.length; ++i)
-		 gnostos.mDocumentElement.innerHTML += titleElements[i].childNodes[0].nodeValue;
-	 
-	 gnostos.mDocumentElement.innerHTML += "</body>";
-	 gDebugger.log(gnostos.mDocumentElement.innerHTML);
-}
-
-gnostos.onXHRError = function(e)
-{
-	gDebugger.log("XHR ERROR!! " + e.target.status);
-}
-
-gnostos.onReadyStateChange = function()
-{
-	gDebugger.log(this.readyState);
-	 if(this.readyState == 4) {
-	 }
 }
 
 var messagepane = document.getElementById("messagepane");
